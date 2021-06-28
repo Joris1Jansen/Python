bd05da1426e44bc7382241b4ae4fb09118aa4b14
Joris <j.jansen@ispnext.com>
2021-06-28 05:20:14 +0200

    Design Patters from the beginning

--- DesignPatterns/Creational/AbstractFactory.py
+++ DesignPatterns/Creational/AbstractFactory.py
@@ -2,40 +2,31 @@ from abc import ABC
 from enum import Enum, auto
 
 class HotDrink(ABC):
-    def consume(self):
+    def consume():
         pass
 
 class Tea(HotDrink):
-    def consume(self):
+    def consume():
         print('This tea is delicious')
 
 class Coffee(HotDrink):
-    def consume(self):
-        print("This coffee is delicious")
+    print('This coffee is delicious')
 
-class HotDrinksFactory(ABC):
+class HotDrinkFactory(ABC):
     def prepare(self, amount):
         pass
 
-class TeaFactory(HotDrinksFactory):
+class TeaFactory(HotDrink):
     def prepare(self, amount):
-        print(f'Put in tea bag, boil wat, '
+        print(f'Put in tea bag, boil water,'
                 f'pour {amount}ml, enjoy!')
-        return Tea()
+        return Tea
 
-class CoffeeFactory(HotDrinksFactory):
+class CoffeeFactory(HotDrink):
     def prepare(self, amount):
-        print(f'Grind some beans, boil water, '
+        print(f'Grind beans, boil water,'
                 f'pour {amount}ml, enjoy!')
-        return Coffee()
-
-def make_drink(types):
-    if types == 'tea':
-        return TeaFactory().prepare(200)
-    elif types == 'coffee':
-        return CoffeeFactory().prepare(50)
-    else:
-        return None
+        return Coffee
 
 class HotDrinkMachine:
     class AvailableDrink(Enum):
@@ -44,16 +35,16 @@ class HotDrinkMachine:
     
     factories = []
     initialized = False
-    
+
     def __init__(self):
         if not self.initialized:
             self.initialized = True
             for d in self.AvailableDrink:
                 name = d.name[0] + d.name[1:].lower()
                 factory_name = name + 'Factory'
-                factory_instace = eval(factory_name)()
-                self.factories.append((name, factory_instace))
-
+                factory_instance = eval(factory_name)()
+                self.factories.append((name, factory_instance))
+    
     def make_drink(self):
         print('Available drinks: ')
         for f in self.factories:
@@ -66,6 +57,14 @@ class HotDrinkMachine:
         return self.factories[idx][1].prepare(amount)
 
 
+
+def make_drink(type):
+    if type == 'tea':
+        return TeaFactory().prepare(200)
+    elif type == 'coffee':
+        return CoffeeFactory().prepare(50)
+    else:
+        return None
 
 if __name__ == '__main__':
     hdm = HotDrinkMachine()
